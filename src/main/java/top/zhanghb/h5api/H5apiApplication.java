412ebfdd15b7bcd28489480bd1581215ae52400b
zhanghuabo_ht@xhqb.com <zhanghuabo_ht@xhqb.com>
2019-07-05 18:26:03 +0800

    aliyun

--- src/main/java/top/zhanghb/h5api/H5apiApplication.java
+++ src/main/java/top/zhanghb/h5api/H5apiApplication.java
@@ -2,8 +2,10 @@ package top.zhanghb.h5api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @SpringBootApplication
+@ConfigurationProperties("secret.yml")
 public class H5apiApplication {
 
     public static void main(String[] args) {
